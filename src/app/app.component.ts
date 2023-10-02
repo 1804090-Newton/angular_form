937c0d9c59da02bdf82242d5e33342bc36faea55
1804090-Newton <u1804090@student.cuet.ac.bd>
2023-10-02 11:17:23 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'newdon';
+  topics=['Angular','react','vue'];
+  name="Newton";
+
+  onButtonPress() {
+    this.name = `${this.name}  Hello world`;
+  }
+  
 }
